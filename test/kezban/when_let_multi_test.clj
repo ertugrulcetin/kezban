b14cde6988dc7df96eb2bb79f531c4b535a30545
ertugrulcetin <ertugrul.cetin@mebitech.com>
2016-03-31 22:18:41 +0300

    when-let-multi test cases added

--- test/kezban/when_let_multi_test.clj
+++ test/kezban/when_let_multi_test.clj
@@ -2,9 +2,34 @@
   (:require [kezban.core :refer :all]
             [clojure.test :refer :all]))
 
+(deftest test-when-let-multi-truthy
+  (is (= (when-let-multi [a 1] a) 1))
+  (is (= (when-let-multi [a 1 b 2] a) 1))
+  (is (= (when-let-multi [a 1 b 2 c true] b) 2))
+  (is (= (when-let-multi [a 1 b 2 c true] c) true)))
 
+(deftest test-when-let-multi-truthy-with-body
+  (is (= (when-let-multi [a 1 b 2]
+                         (println "Hi there sexy!")
+                         (dotimes [_ 3]
+                           (println "Yeah.."))
+                         a) 1))
 
-(deftest addition
-  (is (= 4 (+ 2 2)))
-  (is (= 7 (+ 3 1))))
+  (is (= (when-let-multi [a 1 b 2 c true]
+                         (println "Let's try again!")
+                         "Me Gusto!") "Me Gusto!")))
 
+(deftest test-when-let-multi-falsy
+  (is (= (when-let-multi [a 1 b nil] a) nil))
+  (is (= (when-let-multi [a 1 b 2 c false] b) nil))
+  (is (= (when-let-multi [a 1 b false c 3] a) nil)))
+
+(deftest test-when-let-multi-falsy-with-body
+  (is (= (when-let-multi [a 1 b 2 c false]
+                         (println "Come on run this shit!")
+                         (println "Damn!")
+                         a) nil))
+
+  (is (= (when-let-multi [a 1 b false c 3]
+                         (println "Please?")
+                         a) nil)))
