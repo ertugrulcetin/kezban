04b1e1d2151f81b1211d471db640f4953bf7d2ad
ertugrulcetin <ertugrul.cetin@mebitech.com>
2016-04-02 00:24:29 +0300

    assert func. added

--- src/kezban/core.clj
+++ src/kezban/core.clj
@@ -6,6 +6,15 @@
 
 ;;TODO Write assert-me macro for multiple assertion
 
+(defmacro assert-all
+  [& pairs]
+  `(do (when-not ~(first pairs)
+         (throw (IllegalArgumentException.
+                  (str (first ~'&form) " requires " ~(second pairs) " in " ~'*ns* ":" (:line (meta ~'&form))))))
+       ~(let [more (nnext pairs)]
+          (when more
+            (list* `assert-args more)))))
+
 (defn- get-secondary-values-from-vector
   [bindings]
   (map #(second %) (partition 2 bindings)))
@@ -13,8 +22,9 @@
 (defmacro when-let-multi
   "Multiple binding version of when-let"
   [bindings & body]
-  (assert (vector? bindings) "a vector for its binding")
-  (assert (even? (count bindings)) "exactly even forms in binding vector")
+  (assert-all
+    (vector? bindings) "a vector for its binding"
+    (even? (count bindings)) "exactly even forms in binding vector")
   `(let [r# (and ~@(get-secondary-values-from-vector bindings))]
      (when r#
        (let* ~(destructure bindings) ~@body))))
@@ -24,6 +34,9 @@
   ([bindings then]
    `(if-let-multi ~bindings ~then nil))
   ([bindings then else]
+   (assert-all
+     (vector? bindings) "a vector for its binding"
+     (even? (count bindings)) "exactly even forms in binding vector")
    `(let [temp# (and ~@(get-secondary-values-from-vector bindings))]
       (if temp#
         (let* ~(destructure bindings) ~then)
