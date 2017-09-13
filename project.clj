a1a63c50e7a4982e0573ebafa180a306791f0a77
ertugrulcetin <ertu413@gmail.com>
2017-09-13 09:47:09 +0200

    update version

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject kezban "0.1.7-SNAPSHOT"
+(defproject kezban "0.1.7"
   :description "Extended Collection & Utility library for Clojure"
   :license {:name "Apache License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :url "https://github.com/ertugrulcetin/kezban"
   :dependencies [[org.clojure/clojure "1.8.0"]])
