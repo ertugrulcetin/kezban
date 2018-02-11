07cc4b7ad5c60b4d8067f3023430af492e0d2ae7
ertugrulcetin <ertu413@gmail.com>
2018-02-11 17:43:37 +0300

    added dissoc-in

--- src/kezban/core.clj
+++ src/kezban/core.clj
@@ -288,3 +288,19 @@
   [pred n coll]
   (let [[head tail] (split-with pred coll)]
     (concat head (take n tail))))
+
+
+(defn dissoc-in
+  ([m ks]
+   (if-let [[k & ks] (seq ks)]
+     (if (seq ks)
+       (let [v (dissoc-in (get m k) ks)]
+         (if (empty? v)
+           (dissoc m k)
+           (assoc m k v)))
+       (dissoc m k))
+     m))
+  ([m ks & kss]
+   (if-let [[ks' & kss] (seq kss)]
+     (recur (dissoc-in m ks) ks' kss)
+     (dissoc-in m ks))))
